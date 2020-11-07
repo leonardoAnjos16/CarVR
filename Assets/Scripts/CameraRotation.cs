f1173be0019a0da8966ca2d6d12bb36264d6b273
Leonardo dos Anjos Silva <leonardoanjos.1a2015@gmail.com>
2020-11-07 02:02:48 +0000

    Created camera rotation!

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+    public float speed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Gets current camera rotation
+        Vector3 rotation = transform.eulerAngles;
+
+        // Calculates desired rotation angle
+        float angle = Input.GetAxis("Mouse X") * Time.deltaTime * speed;
+
+        // Ensures it rotates within interval [-90, 90]
+        rotation.y += angle;
+        if (rotation.y > 90f && rotation.y < 270f) {
+            if (angle < 0) rotation.y = 270f;
+            else if (angle > 0) rotation.y = 90f;
+        }
+
+        // Change camera rotation
+        transform.eulerAngles = rotation;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,34 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
     private Rigidbody rb;
-    private float speed;
     private float dirX;
 
+    public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Set speed for player 
-        speed = 3f;
-
         //Get rigidbody of the object that has the script
-        rb =  GetComponent<Rigidbody>();  
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //Getting the axis the user clicked and multiplying by the speed
-        dirX = Input.GetAxis("Horizontal") * speed;   
+        dirX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
     }
-
 
     //Usually you change the physics in this function
     void FixedUpdate(){
-
-        //Changing the velocity of the objec
+        //Changing the velocity of the object
         rb.velocity = new Vector3(dirX, 0, 0);
     }
 }
