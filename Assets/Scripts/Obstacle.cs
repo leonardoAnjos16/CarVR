ddc3b8cf6e302070c583fb7ea1125a5816baf92e
Leonardo dos Anjos Silva <leonardoanjos.1a2015@gmail.com>
2020-11-14 19:58:11 +0000

    Created first type of obstacle (car)!

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private GameController gameController;
+    private Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = FindObjectOfType(typeof(GameController)) as GameController;
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z < -20f)
+            Destroy(gameObject);
+    }
+
+    void FixedUpdate() {
+        rigidBody.velocity = new Vector3(0f, 0f, -gameController.speed);
+    }
+}
