099cbe527deeed985827ca92a57fa1d63bbdd1e9
Leonardo dos Anjos Silva <leonardoanjos.1a2015@gmail.com>
2020-11-15 06:13:14 +0000

    Changed car movement logic!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,30 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private GameController gameController;
-    private Rigidbody rigidBody;
-    private float speed;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Gets GameController object from the scene
-        gameController = FindObjectOfType(typeof(GameController)) as GameController;
-
-        // Gets rigidbody of the object that has the script
-        rigidBody = GetComponent<Rigidbody>();
-    }
-
     // Update is called once per frame
     void Update()
     {
-        // Gets the axis the user clicked and multiply it by the speed
-        speed = Input.GetAxis("Horizontal") * gameController.speed;
+        Vector3 position = transform.position;
+
+        if (Input.GetKeyUp("left") || Input.GetKeyUp("a")) position.x -= 6f;
+        if (Input.GetKeyUp("right") || Input.GetKeyUp("d")) position.x += 6f;
+
+        position.x = Mathf.Clamp(position.x, -6f, 6f);
+        transform.position = position;
     }
 
-    // Usually you make physics-related changes in this function
-    void FixedUpdate(){
-        //Changing the velocity of the object
-        rigidBody.velocity = new Vector3(speed, 0, 0);
+    void OnCollisionEnter(Collision collision) {
+        // Do gameover logic
     }
 }
