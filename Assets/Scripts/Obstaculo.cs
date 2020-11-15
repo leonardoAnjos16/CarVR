f835be044b0f39cb360f29dff84a3cd626b29926
Guilherme Xavier <gfxs@cin.ufpe.br>
2020-11-15 03:07:51 +0000

    Added rearview mirror and upper mirror

---
+++ Assets/Scripts/Obstaculo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstaculo : MonoBehaviour
+{
+    public float speedMultiplier;
+    public Rigidbody rb;
+    private GameController gameController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = FindObjectOfType(typeof(GameController)) as GameController;
+        Debug.Log("Funcionando pelo menos isso");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= new Vector3(0, 0, 0.2f + gameController.speed);
+    }
+}
