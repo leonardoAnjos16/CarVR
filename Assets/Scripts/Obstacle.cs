7714d7b70190a91e7dbec22034614de89c7a63ad
Leonardo dos Anjos Silva <leonardoanjos.1a2015@gmail.com>
2020-11-15 01:12:19 +0000

    Updated obstacle destroy position!

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -19,7 +19,7 @@ public class Obstacle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z < -20f)
+        if (transform.position.z < -100f)
             Destroy(gameObject);
     }
 
