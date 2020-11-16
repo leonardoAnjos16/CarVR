ed058c4213cca9ff4b8c780c00eb9e9e7566acb7
Elaine Cruz <cruzelainef@gmail.com>
2020-11-16 16:15:48 +0000

    Adding part of the score logic

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,12 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GameController : MonoBehaviour
 {
+    private int score = 0;
+    private int highScore;
+    public Text scoreText;
+    public Text highScoreText;
+
+    void Start(){
+        scoreText.text = "Score: 0";
+
+        if(PlayerPrefs.HasKey("highscore")){
+            highScore = PlayerPrefs.GetInt("highscore");
+            highScoreText.text = "High Score: " +  highScore;
+
+        }else{
+              highScoreText.text = "Score: 0";
+              highScore = 0;
+        }
+    }
+
+    void Update(){
+        score = score + 1;
+    }
+
+
     public void GameOver(string message) {
         // TODO: Change to actual game over logic
         Time.timeScale = 0f;
         Debug.Log("Game Over! " + message);
+
+        
+    }
+
+    private void CheckHighScore(){
+       if(score>highScore){
+           PlayerPrefs.SetInt("highscore", score);
+       }
     }
 }
