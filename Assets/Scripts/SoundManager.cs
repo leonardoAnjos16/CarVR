619ba9530aeaf631cfcecf3b4a8327c1a5f7d979
Elaine Cruz <cruzelainef@gmail.com>
2020-11-28 15:37:31 +0000

    FinalAdjustments

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    GameObject[] objs;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        objs = GameObject.FindGameObjectsWithTag("Music");
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
